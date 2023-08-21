54c1d52a6def0aad9818b1e0422beaf452ccb5d1
Guilherme Viana <imluizguilara@hotmail.com>
2023-08-21 20:01:59 -0300

    Added local storage

--- src/contexts/PostsContext.tsx
+++ src/contexts/PostsContext.tsx
@@ -1,10 +1,10 @@
-import { Dispatch, ReactNode, createContext, useReducer, useState } from "react";
+import { Dispatch, ReactNode, createContext, useEffect, useReducer, useState } from "react";
 
-import { PostProps } from "@/@types/Post";
+import { Post } from "@/@types/Post";
 import { PostsReducer } from "@/reducers/PostsReducer";
 
 type PostsContext = {
-    posts: PostProps[]
+    posts: Post[]
     dispatch: Dispatch<any>
     editPostModal: boolean
     setEditPostModal: (modalState: boolean) => void
@@ -12,11 +12,19 @@ type PostsContext = {
 
 export const PostsContext = createContext<PostsContext | null>(null)
 
+const STORAGE_KEY = 'postContextContent'
 
 type Props = { children: ReactNode }
 export function PostsProvider({ children }: Props) {
     const [editPostModal, setEditPostModal] = useState(false);
-    const [posts, dispatch] = useReducer(PostsReducer, []);
+    const [posts, dispatch] = useReducer(
+        PostsReducer,
+        JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')       // para a execução se a primeira condição for satisfeita. [] = valor inicial
+    );
+
+    useEffect(() => {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(posts))
+    }, [posts])
 
     
     return (
